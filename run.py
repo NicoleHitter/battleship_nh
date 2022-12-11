1f9148a0c7523c2c4c23ba1b1b4e2d5dccba3ac6
NicoleHitter <nicole.hitter92@gmail.com>
2022-12-11 15:37:01 +0000

    Add function that will request ship coordinates input from user and then generate the new board

--- run.py
+++ run.py
@@ -1,14 +1,19 @@
 from random import randint
 
 board = []
-elements = []
+b=[]
+new_board = []
+c=0
+r=0
 """
 Welcome message 
 """
 print("Welcome to Battleship!")
 
 """
-Request user to select number of rows and columns
+Request user to select a number 
+that will give the number of rows and columns 
+for the board 
 """
 while True:
     try:
@@ -26,6 +31,7 @@ def create_board(board_size):
     """
     Creates the board using the board_size number 
     """
+
     for x in range(board_size):
         row = []
         for y in range(board_size):
@@ -34,23 +40,41 @@ def create_board(board_size):
     return board
 
 
-def display_board(board, board_size):
+def display_board(b, board_size):
     """
     Iterating through list elements and displaying them 
     in rows of the board_size length
     """
+    elements = []
     for x in range(board_size):
         for y in range(board_size):
-            elements.append(board[x][y])
+            elements.append(b[x][y])
     
-    print(elements)
-
     for i in range(0,len(elements),board_size):
         print(*elements[i:i+board_size])
+        
    
-
+def get_ship_coordinates(board,c,r):
+    """
+    Request user to give two numbers
+    that will be the ship coordinates  
+    """
+    print("Please select the position of your ship whithin the board by choosing the column and row where this is located.")
+    c = int(input())
+    r = int(input())
+    new_board = board
+    new_board[c-1][r-1] = "@"
+    return new_board
 
 
 create_board(board_size)
 display_board(board,board_size)
+new_board = get_ship_coordinates(board,c,r)
+display_board(new_board,board_size)
+
+
+
+
+
+
 
