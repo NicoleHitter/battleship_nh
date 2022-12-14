067b5857c7a400d00a23384f86bc01773c0c1a6e
NicoleHitter <nicole.hitter92@gmail.com>
2022-12-14 22:12:27 +0000

    Rephrase docstrings and fix bugs

--- run.py
+++ run.py
@@ -4,24 +4,29 @@ from random import randint
 
 def display_instructions():
     """
-    This functions is going to display 
-    welcome messafe and instructions 
+    This function is going to display 
+    welcome message and instructions 
     for the player
     """
     print("Welcome to Battleship-NH!\n")
     print("First you will be asked to introduce a number,")
     print("that will be the size of the board,")
-    print("and then the row and column where you will throw the missile.")
-    print("Once this is done, you will be displayed message")
+    print("and then the row and column where you will throw the missile,")
+    print("couting for these starts with 0.")
+    print("Once this is done, you will be displayed a message")
     print("to confirm or not if you managed to hit the ship.")
 
 
 def get_board_size():
     """
-    Request user to select a number 
-    that will give the size of the board
+    Requests input from user,
+    then input needs to be checked
+    if it is a number and within 
+    the board size, if so this will 
+    be transformed into integer and 
+    returned
     """
-    print("Please enter a number between 4 and 9.")
+    print("Please choose the board size by entering a number between 4 and 9.")
 
     while True:
         board_size = input()
@@ -31,15 +36,17 @@ def get_board_size():
         
         else:
             print("Invalid number! Please try again.")
-            
-        
+
     return board_size
 
 
 def display_board(board_size, missile_coordinates=None, ship_coordinates=None):
     """
-    Iterating through list elements and displaying them 
-    in rows of the board_size length
+    This function takes the board_size,
+    then generates a board that has equal 
+    rows and columns to it, and places 
+    "M" and "S" at their specific coordinates, 
+    once these are declared
     """
     
     for row in range(board_size):
@@ -52,53 +59,55 @@ def display_board(board_size, missile_coordinates=None, ship_coordinates=None):
             else:
                 print(". ", end=' ')
         print("\n")
+
+def get_random_ship(board_size):
+    """
+    This function will generate two random numbers,
+    that return them as a tuple
+    """
+    ship_row = random.randint(0, board_size-1)
+    ship_column = random.randint(0, board_size-1)
+    
+    return (ship_row, ship_column)
         
    
 def get_missile_coordinates(board_size):
     """
-    Request user to give two numbers
-    that will be the player's guessed coordinates  
-    for the ship
-    which before beeing passed to new_board 
-    will need to be validated 
+    This function takes the board_size,
+    then asks user to input two values, 
+    values that need to be a number 
+    between 0 and board_size.
     """
-    print(f"Please select the position of your missile whithin the board by first choosing the row where this will land.\nNumbers need to be integers and within 1-{board_size} interval.")
+
+    print(f"Please select the position of your missile whithin the board, by first choosing the row where this will land.\nNumbers need to be integers and within 0-{board_size-1} interval.")
+    
     while True:
         missile_row = input()
-        if (missile_row.isnumeric() and 1 <= int(missile_row) < board_size and 1<= int(missile_row) < board_size):
+        if (missile_row.isnumeric() and 0 <= int(missile_row) < board_size and 0 <= int(missile_row) < board_size):
             missile_row = int(missile_row)
             break
         else:
             print("Invalid number! Please try again.")
 
-    print(f"Please select the position of your missile whithin the board by first choosing the column where this will land.\nNumbers need to be integers and within 1-{board_size} interval.")
+    print(f"Please select the position of your missile whithin the board by now choosing the column where this will land.\nNumbers need to be integers and within 1-{board_size-1} interval.")
+    
     while True:
         missile_column = input()
-        if (missile_column.isnumeric() and 1 <= int(missile_column) < board_size and 1<= int(missile_column) < board_size):
+        if (missile_column.isnumeric() and 0 <= int(missile_column) < board_size and 0 <= int(missile_column) < board_size):
             missile_column = int(missile_column)
             break
         else:
             print("Invalid number! Please try again.")         
   
     return (missile_row, missile_column)
-
-
-def get_random_ship():
-    """
-    This function will generate two random numbers 
-    that will be the real ship coordinates
-    """
-    ship_row = random.randint(1,board_size)
-    ship_column = random.randint(1,board_size)
-    
-    return (ship_row, ship_column)
     
 
 def check_sinking(missile_coordinates, ship_coordinates):
     """
-    This function checks if ship has been hit 
-    by checking if the coordinates of the ship
-    are the same coordinates with the missile
+    This function takes two tuples,
+    missile_coordinates and ship_coordinates),
+    compares them and then print a message
+    accordingly
     """
     if missile_coordinates == ship_coordinates:
         print(f"Ship has been sinked!You won this battle!You have thrown missile at:{missile_coordinates} exactly were ship was located")
@@ -114,15 +123,12 @@ def play_game():
     """
     display_instructions()
     board_size = get_board_size()
+    print(board_size)
     board = display_board(board_size)
-    missile_coordinates = get_missile_coordinates()
-    print(missile_coordinates)
-    ship_coordinates = get_random_ship()
-    print(ship_coordinates)
+    ship_coordinates = get_random_ship(board_size)
+    missile_coordinates = get_missile_coordinates(board_size) 
     check_sinking(missile_coordinates, ship_coordinates)
     board = display_board(board_size, missile_coordinates, ship_coordinates)
 
 
-
-missile_coordinates = get_missile_coordinates(5)
-print(missile_coordinates)
+play_game()
